03db0b9fb1d451b14ee7d8237674fd5893d1ec8b
Mustaphanop <154910658+Mustaphanop@users.noreply.github.com>
2024-02-26 02:36:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Team-Alisha/Hero"
+    "UPSTREAM_REPO", "https://github.com/mustaphanop/mu"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
